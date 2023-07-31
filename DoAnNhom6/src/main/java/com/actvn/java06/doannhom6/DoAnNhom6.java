10fd5661f2bed4d070bc6f7c743fc3a334327657
CT070226-HOANGVANHUY <hoanghuy10a1gtc@gmail.com>
2023-07-31 23:53:11 +0700

    Huy Hoang nhap du lieu

--- DoAnNhom6/src/main/java/com/actvn/java06/doannhom6/DoAnNhom6.java
+++ DoAnNhom6/src/main/java/com/actvn/java06/doannhom6/DoAnNhom6.java
@@ -4,6 +4,8 @@
 
 package com.actvn.java06.doannhom6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
@@ -11,7 +13,7 @@ package com.actvn.java06.doannhom6;
 public class DoAnNhom6 {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello nhom 6!");
     }
 }
